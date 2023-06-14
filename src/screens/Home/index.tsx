cf4e43e02d5dbb6ad0ab3c7019cc0c73224febbf
AllanLobato <allandonizetti@gmail.com>
2023-06-14 18:08:16 -0300

    first commit

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,90 @@
+import { useState } from "react";
+import {
+  Alert,
+  FlatList,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
+
+import { Participant } from "../components/Participant";
+
+import { styles } from "./style";
+
+export function Home() {
+  const [participants, setParticipants] = useState<string[]>([]);
+  const [participantName, setParticipantName] = useState("");
+
+  function handleParticipantAdd() {
+    //adiciona participante na lista
+    if (participants.includes(participantName)) {
+      return Alert.alert(
+        "Participante existe",
+        "Já existe um participante na lista com esse nome."
+      );
+    }
+
+    setParticipants((prevState) => [...prevState, participantName]); //adiciona o nome do participante no estado
+    setParticipantName("");
+  }
+
+  function handleParticipantRemove(name: string) {
+
+    Alert.alert("Remover", `Remover o participante ${name}?`, [
+      {
+        text: "Sim",
+        onPress: () =>
+          setParticipants((prevState) =>
+            prevState.filter((participant) => participant !== name)
+          ), //filtra os participantes e remove o que foi clicado
+      },
+      {
+        text: "Não",
+        style: "cancel",
+      },
+    ]);
+  }
+
+  // OS ESTADOS NO REACT SÃO ASSINCRONOS, OU SEJA, NÃO SÃO ATUALIZADOS NA HORA. POR ISSO, QUANDO QUEREMOS USAR O VALOR ATUALIZADO DE UM ESTADO, USAMOS O USEEFFECT
+  return (
+    <View style={styles.container}>
+      <Text style={styles.eventName}>Nome do evento</Text>
+
+      <Text style={styles.eventDate}>Sexta, 4 de Novembro de 2022.</Text>
+
+      <View style={styles.form}>
+        <TextInput
+          style={styles.input}
+          placeholder="Nome do participante"
+          placeholderTextColor="#6B6B6B"
+          onChangeText={setParticipantName} //transfere automaticamente o texto para o estado
+          value={participantName} //valor do estado
+        />
+
+        <TouchableOpacity style={styles.button} onPress={handleParticipantAdd}>
+          <Text style={styles.buttonText}>+</Text>
+        </TouchableOpacity>
+      </View>
+
+      <FlatList
+        data={participants}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => (
+          <Participant
+            key={item}
+            name={item}
+            onRemove={() => handleParticipantRemove(item)}
+          />
+        )}
+        showsVerticalScrollIndicator={false}
+        ListEmptyComponent={() => (
+          <Text style={styles.listEmptyText}>
+            Ninguém chegou no evento ainda? Adicione participantes a sua lista
+            de presença.
+          </Text>
+        )}
+      />
+    </View>
+  );
+}
